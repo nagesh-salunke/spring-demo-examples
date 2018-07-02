83ac52306a5d9e2e17e327d6241ad0028b231a51
Nagesh Salunke <salunkenagesh14@gmail.com>
2018-07-02 22:39:04 +0530

    Add basic spring mvc controller and tests

---
+++ spring-webmvc-demo/src/test/java/com/example/demo/HomePageControllerTests.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomePageControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void validateGreetings() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("greeting"))
+				.andExpect(model().attributeExists("name"));
+	}
+
+}
