51ba22e20376cb27b179617a37ed7777e3f9e678
nagesh-salunke <salunkenagesh14@gmail.com>
2018-07-04 13:49:34 +0530

    Issue dashboard initial commit

---
+++ issue-dashboard/src/main/java/com/springdemo/issuedashboard/issuedashboard/IssueDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.springdemo.issuedashboard.issuedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueDashboardApplication.class, args);
+	}
+}
---
+++ issue-dashboard/src/test/java/com/springdemo/issuedashboard/issuedashboard/IssueDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springdemo.issuedashboard.issuedashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssueDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
